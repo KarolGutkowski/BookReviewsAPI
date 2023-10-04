61fade5e40792dd0dcc329baac3066250044613c
Karol Gutkowski <karol.gutkowski.stud@pw.edu.pl>
2023-10-04 20:40:40 +0200

    modified model attributes to serialize only when authors and reviews are deliberately included

--- BookReviewsAPI/Models/DataModels/Book.cs
+++ BookReviewsAPI/Models/DataModels/Book.cs
@@ -6,18 +6,14 @@ namespace BookReviewsAPI.Models
     public class Book
     {
         public int Id {get; set;}
-
         [Required]
         public string Title { get; set; }
         public int Year { get; set; }
-
-        [JsonIgnore]
-        public ICollection<Author> Authors { get; set; }
-
-        [JsonIgnore]
-        public ICollection<Review> Reviews { get; set; }
-
-        public string Img { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public ICollection<Author>? Authors { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public ICollection<Review>? Reviews { get; set; }
+        public string? Img { get; set; }
     }
 
 }
