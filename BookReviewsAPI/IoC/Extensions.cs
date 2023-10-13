7c6b6c93cc756d99f779bc9895c97bf3f6d6749b
Karol Gutkowski <karol.gutkowski.stud@pw.edu.pl>
2023-10-13 19:29:04 +0200

    some cors changes

--- BookReviewsAPI/IoC/Extensions.cs
+++ BookReviewsAPI/IoC/Extensions.cs
@@ -74,7 +74,7 @@ namespace Microsoft.Extensions
                 options.AddPolicy(name: CorsPolicyConsts.AllowLocalhostClient,
                                   policy =>
                                   {
-                                      policy.WithOrigins("http://localhost:3000")
+                                      policy.WithOrigins("https://localhost:3000")
                                       .AllowAnyHeader()
                                       .AllowAnyMethod()
                                       .AllowCredentials();
