8c2f473467aa7354cec34eb93f4d24c64f755b3c
Karol Gutkowski <karol.gutkowski.stud@pw.edu.pl>
2023-10-14 17:04:34 +0200

    further frontend styling modifications

--- BookReviewsClient/bookreviewsclient/src/Components/NavigationBar.jsx
+++ BookReviewsClient/bookreviewsclient/src/Components/NavigationBar.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import UserLoginStateContext from "./UserLoginStateContext";
+import searchIcon from "../images/search-icon.png";
 
 const NavigationBar = ()=>
 {
@@ -7,14 +8,31 @@ const NavigationBar = ()=>
     const item = isLoggedIn? 
     <nav>
       <ul className="nav-list">
-        <li className="nav-item">Home</li>
+        <li>
+          <li className="nav-item">Home</li>
+          <li className="nav-item">Catalog</li>
+        </li>
+        <li>
+          <input type="text" placeholder="Search By Title or Author"/>
+        </li>
         <li className="nav-item">Logout</li>
       </ul>
     </nav> : 
     <nav>
         <ul className="nav-list">
           <li className="nav-item">Home</li>
-          <li className="nav-item">Login</li>
+          <li className="nav-item">Catalog</li>
+          <li className="nav-item">
+            <form className="search-bar">
+              <button type="submit" className="search-icon-button">
+                <img src={searchIcon} alt="search-icon" className="search-icon"/>
+              </button>
+            <input type="text" className="nav-search-bar" placeholder="Search By Title or Author"/>
+            </form>
+          </li>
+          <li className="nav-item">
+              <button className="login-button">Login</button>
+          </li>
         </ul>
     </nav>
     
