2f58f1909785d4a81030c4e196e3ee2461822c38
Karol Gutkowski <karol.gutkowski.stud@pw.edu.pl>
2023-10-17 19:53:29 +0200

    another unit test of registration system

--- BookReviews.UnitTests/RegistrationTests.cs
+++ BookReviews.UnitTests/RegistrationTests.cs
@@ -8,35 +8,48 @@ using System.Collections.Generic;
 using Moq.EntityFrameworkCore;
 using BookReviews.Infrastructure.Registration;
 using System.Linq;
+using System.Linq.Expressions;
+using System;
 
 namespace BookReviews.UnitTests
 {
     public class RegistrationTests
     {
-        [Fact]
-        public void RegistrationHelper_ShouldAddNewUserWhenNoOtherExists()
+        Mock<IPasswordCryptographyHelper> mockCryptographyHelper;
+        List<User> inMemoryUsersList;
+        Mock<IUsersRepository> mockUsersRepository;
+        public RegistrationTests()
         {
-            // arrange
-            var mockCryptographyHelper = new Mock<IPasswordCryptographyHelper>();
+            mockCryptographyHelper = new Mock<IPasswordCryptographyHelper>();
             mockCryptographyHelper
                 .Setup(s => s.GenerateHash(It.IsAny<string>()))
                 .Returns<string>(x => x);
-
             mockCryptographyHelper
                 .Setup(s => s.VerifyPassword(It.IsAny<string>(), It.IsAny<string>()))
                 .Returns((string x, string y) => x == y);
 
-            var usersList = new List<User>();
+            inMemoryUsersList = new List<User>();
 
-            var mockUsersRepository = new Mock<IUsersRepository>();
+            var dbSetMock = new Mock<DbSet<User>>();
+            dbSetMock.As<IQueryable<User>>().Setup(m => m.Provider).Returns(inMemoryUsersList.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<User>>().Setup(m => m.Expression).Returns(inMemoryUsersList.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(inMemoryUsersList.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(inMemoryUsersList.AsQueryable().GetEnumerator());
+
+            mockUsersRepository = new Mock<IUsersRepository>();
             mockUsersRepository
                 .Setup(s => s.Users)
-                .ReturnsDbSet(usersList);
+                .ReturnsDbSet(dbSetMock.Object);
 
             mockUsersRepository
-                .Setup(s => s.Users.Add(It.IsAny<User>()))
-                .Callback<User>(s => usersList.Add(s));
+               .Setup(s => s.Users.Add(It.IsAny<User>()))
+               .Callback<User>(s => inMemoryUsersList.Add(s));
+        }
 
+        [Fact]
+        public void RegistrationHelper_ShouldAddNewUserWhenNoOtherExists()
+        {
+            // arrange
             var registrationHelper = new RegistrationHelper(mockUsersRepository.Object, mockCryptographyHelper.Object);
 
             var username = "testUser";
@@ -44,14 +57,35 @@ namespace BookReviews.UnitTests
 
             // act
             var result = registrationHelper.TryToRegisterUser(username, password);
-            var newUser = usersList.SingleOrDefault();
+            var newUser = inMemoryUsersList.SingleOrDefault();
             // assert
 
-            Assert.Equal(password, newUser.Password);
-            Assert.True(result);
-            Assert.Single(usersList);
             Assert.NotNull(newUser);
-            Assert.Equal(username, newUser.UserName);
+            Assert.Equal(password, newUser?.Password);
+            Assert.True(result);
+            Assert.Single(inMemoryUsersList);
+            Assert.NotNull(newUser);
+            Assert.Equal(username, newUser?.UserName);
+        }
+
+        [Fact]
+        public void RegistrationHelper_ShouldntRegisterExistingUser()
+        {
+            // arrange
+            var existingUser = new User
+            {
+                UserName = "user",
+                Password = "1234"
+            };
+            inMemoryUsersList.Add(existingUser);
+
+            var registrationHelper = new RegistrationHelper(mockUsersRepository.Object, mockCryptographyHelper.Object);
+            // act
+            var result = registrationHelper.TryToRegisterUser(existingUser.UserName, existingUser.Password);
+            var usersCount = inMemoryUsersList.Count();
+            // assert
+            Assert.False(result);
+            Assert.Equal(1, usersCount);
         }
     }
 }
