2005e153092c79dd0f80a8cd4c9f41f44b4b85e6
Karol Gutkowski <01171298@pw.edu.pl>
2023-12-23 22:00:51 +0100

    changes to add book form

--- BookReviewsClient/bookreviewsclient/src/Components/AddBookForm.jsx
+++ BookReviewsClient/bookreviewsclient/src/Components/AddBookForm.jsx
@@ -10,26 +10,31 @@ const AddBookForm = () =>
         //const fileInput = document.getElementById("book-cover-input");
        //const file = fileInput[0];
         const formData = new FormData(form);
-        
-        await fetch(`${config.url}/api/v1/books`,{
+        debugger;
+        const result = await fetch(`${config.url}/api/v1/books`,{
             method: 'POST',
             credentials: "include",
             body: formData
         });
+
+        if(result.ok)
+        {
+            form.reset();
+        }
     }
 
     return (<>
         <form id="book-add-form" className="add-books-form" onSubmit={handleBookAddOnSubmit}>
-            <input type="text" placeholder="Title" name="Title" className="app-input"/>
-            <input type="number" placeholder="Year" name="Year" className="app-input"/>
-            <textarea placeholder="Description" name="Description" className="app-input"/>
-            <input type="text" placeholder="AuthorFirstName" name="AuthorFirstName" className="app-input"/>
-            <input type="text" placeholder="AuthorLastName" name="AuthorLastName" className="app-input"/>
+            <input type="text" placeholder="Title" name="Title" className="app-input" required/>
+            <input type="number" placeholder="Year" name="Year" className="app-input" required/>
+            <textarea placeholder="Description" name="Description" className="app-input" required/>
+            <input type="text" placeholder="AuthorFirstName" name="AuthorFirstName" className="app-input" required/>
+            <input type="text" placeholder="AuthorLastName" name="AuthorLastName" className="app-input" required/>
 
             <label htmlFor="BookCover">
                 Book cover image:
             </label>
-            <input type="file" name="BookCover" className="app-file-input" id="book-cover-input" accept="image/*"/>
+            <input type="file" name="BookCover" className="app-file-input" id="book-cover-input" accept="image/*" required/>
             <button>Send</button>
         </form>
     </>);
